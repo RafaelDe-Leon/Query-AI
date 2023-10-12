13979d3aca7a7c049a1bd6b560f0d3d2de100402
Rafael De Leon <Rafael.deleon12688@gmail.com>
2023-10-11 23:36:05 -0400

    added db to the frontend using a fake server

--- test.js
+++ test.js
@@ -10,3 +10,61 @@ function revealContent(button, contentId) {
     button.textContent = 'Show Advanced Filters'
   }
 }
+
+async function fetchDataAndCreateElements() {
+  try {
+    // Fetch data from your database or server
+    const response = await fetch('http://localhost:3000/db')
+    if (!response.ok) {
+      throw new Error('Network response was not ok')
+    }
+    const data = await response.json()
+
+    // Loop through the data to create HTML elements
+    for (const key in data.name) {
+      const name = data.name[key]
+      const author = data.author[key]
+
+      const section = document.createElement('section')
+      section.classList.add('queries-container')
+
+      const queriesDiv = document.createElement('div')
+      queriesDiv.classList.add('queries')
+
+      const nameParagraph = document.createElement('p')
+      nameParagraph.classList.add('queries-heading')
+      nameParagraph.textContent = name
+
+      const authorParagraph = document.createElement('p')
+      authorParagraph.classList.add('queries-text')
+      authorParagraph.textContent = `Author: ${author}`
+
+      queriesDiv.appendChild(nameParagraph)
+      queriesDiv.appendChild(authorParagraph)
+
+      const logoDiv = document.createElement('div')
+      logoDiv.classList.add('queries-logo')
+
+      // You can add logic here to create and append the logo images as needed
+      // For simplicity, we'll add five identical images
+      for (let i = 0; i < 5; i++) {
+        const img = document.createElement('img')
+        img.src =
+          'https://www.freepnglogos.com/uploads/logo-ig-png/logo-ig-png-instagram-logo-camel-productions-website-25.png'
+        img.alt = 'logo'
+        img.style.width = '20px'
+        logoDiv.appendChild(img)
+      }
+
+      section.appendChild(queriesDiv)
+      section.appendChild(logoDiv)
+
+      document.body.appendChild(section)
+    }
+  } catch (error) {
+    console.error('Error:', error)
+  }
+}
+
+// Call the fetchDataAndCreateElements function to fetch data and create HTML elements
+fetchDataAndCreateElements()
